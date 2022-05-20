80a8e7dc76b425f91486454a6f9110298263f5a1
bryan <bryanfeliciano892@gmail.com>
2022-05-20 12:41:57 -0500

    addUser added

--- app/Main.hs
+++ app/Main.hs
@@ -16,7 +16,31 @@ data Tool = Tool
                 timesBorrowed :: Int
             }
 
-data User 
+data User = User
+            {
+                userId :: Int,
+                userName :: String
+            }
+
+instance Show User where
+    show user = mconcat [show $ userId user , ".) ", userName user]
+
+instance Show Tool where
+    show tool = mconcat [show $ toolId tool, ".) ", name tool , "\n description : ", description tool, "\n last returned: ", show $ lastReturned tool, "\n times borrowed: " , show $ timesBorrowed tool, "\n"]
+
+addUser :: String -> IO()
+addUser userName = do
+    conn <- "tools.db"
+    execute conn "INSERT INTO users (username) VALUES (?)"
+      (Only userName)
+    print "user added"
+    close conn
+
+withConn :: String -> (Connection -> IO ()) -> IO ()
+withConn dbName action = do
+     conn <- open dbName
+     action conn
+     close conn
 
 main :: IO ()
 main = print "db-lesson"
